ab0e2280a623b415f01ad7a10342ee19b36dedbd
amarNrddn <amarnrddn02@gmail.com>
2024-11-23 08:42:33 +0700

    feat: make footer

---
+++ app/(root)/create/event/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const page = () => {
+   return (
+      <div>page create Event</div>
+   )
+}
+
+export default page
--- components/shared/Footer.tsx
+++ components/shared/Footer.tsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import Link from 'next/link'
+import Image from 'next/image'
 
 const Footer = () => {
    return (
-      <div>Footer</div>
+      <footer className='border-t'>
+         <div className="flex-center wrapper flex flex-col gap-4 p-5 text-center md:flex-row">
+            <Link href={'/'}>
+               <Image
+                  src={'/assets/images/logo.png'}
+                  width={60}
+                  height={30}
+                  alt='logo'
+               />
+            </Link>
+            <p>2024 Marzkyy. All Rights reserved</p>
+         </div>
+      </footer>
    )
 }
 
